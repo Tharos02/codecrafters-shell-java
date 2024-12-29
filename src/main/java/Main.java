015e8625315da8a920940020ce73c234ac3f97cb
Till Weckheuer <till.weckheuer@aol.com>
2024-12-29 16:45:37 +0100

    REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
         System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        if(!input.isEmpty()){
+        do {
             System.out.println(input + ": command not found");
-        }
+            System.out.print("$ ");
+
+            input = scanner.nextLine();
+
+        } while (!input.isEmpty());
+        scanner.close();
     }
 }
